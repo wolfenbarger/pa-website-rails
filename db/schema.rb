3842f6acfb8291f542fbb96b6fddbfc5297dac1c
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-09-29 11:51:47 -0700

    Fixed local db schema... Should this be ignored?

--- db/schema.rb
+++ db/schema.rb
@@ -16,30 +16,6 @@ ActiveRecord::Schema.define(version: 20150920202124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "QuizDescriptors", force: :cascade do |t|
-    t.jsonb    "descriptor"
-    t.string   "title",         limit: 255, default: ""
-    t.boolean  "hidden",                    default: false
-    t.boolean  "published",                 default: false
-    t.datetime "createdAt",                                 null: false
-    t.datetime "updatedAt",                                 null: false
-    t.integer  "UserAwesomeId"
-  end
-
-# Could not dump table "Users" because of following StandardError
-#   Unknown type '"enum_Users_role"' for column 'role'
-
-  create_table "tickets", force: :cascade do |t|
-    t.string   "name"
-    t.string   "seat_id_seq"
-    t.text     "address"
-    t.decimal  "price_paid"
-    t.string   "email_address"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.string   "phone"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -61,5 +37,4 @@ ActiveRecord::Schema.define(version: 20150920202124) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "QuizDescriptors", "\"Users\"", column: "UserAwesomeId", primary_key: "awesome_id", name: "QuizDescriptors_UserAwesomeId_fkey", on_update: :cascade, on_delete: :nullify
 end
